9b37ef9132fc61f33a8c1bf560074d61c6a5da76
Ashish Shrestha <ashish.shrestha@truenary.com>
2025-06-02 15:51:15 +0545

    feat(Program.cs): implement driver code

--- App/Program.cs
+++ App/Program.cs
@@ -1,7 +1,46 @@
-﻿class Program
+﻿using App.Anaconda;
+using App.Animal;
+using App.Eagle;
+using App.Enclosure;
+using App.Lion;
+using App.Zebra;
+using App.ZooKeeper;
+
+class Program
 {
     static void Main(String[] args)
     {
+        Lion lion1 = new Lion("Aaron", "Lion", 22);
+        Lion lion2 = new Lion("Baron", "Lion", 32);
+        Lion lion3 = new Lion("Caron", "Lion", 42);
 
+        Enclosure<Lion> lionLand = new Enclosure<Lion>();
+
+        lionLand.AddAnimal(lion1);
+        lionLand.AddAnimal(lion2);
+        lionLand.AddAnimal(lion3);
+        lionLand.DisplayAnimalsDetails();
+        lionLand.PerformRoutineCheckup();
+
+        ZooKeeper admin1 = new ZooKeeper();
+        admin1.FeedAnimalInEnclosure(lionLand, "meat");
+
+        Lion lion = new Lion("Leo", "Lion", 30);
+        Zebra zebra = new Zebra("Zerren", "Zebra", 32);
+        Eagle eagle = new Eagle("Erry", "Eagle", 42);
+        Anaconda anaconda = new Anaconda("Arr", "Anaconda", 42);
+
+        Enclosure<Animal> mixedHabitat = new Enclosure<Animal>();
+
+        mixedHabitat.AddAnimal(lion);
+        mixedHabitat.AddAnimal(zebra);
+        mixedHabitat.AddAnimal(eagle);
+        mixedHabitat.AddAnimal(anaconda);
+
+        mixedHabitat.DisplayAnimalsDetails();
+        Console.WriteLine(mixedHabitat.GetAnimalCount());
+
+        ZooKeeper admin2 = new ZooKeeper();
+        admin2.ObserveMotionsInEnclosure(mixedHabitat);
     }
 }
